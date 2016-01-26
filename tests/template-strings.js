28400e89ae4dd0cdaf4e232cd123514c81a8dc69
schmitz1193 <schmitz1193@gmail.com>
2016-01-25 18:48:27 -0600

    tests clear for es6

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -2,7 +2,7 @@ var expect=require('chai').expect;
 
 describe('template strings', function() {
   it('true should be a string', function() {
-    var string = 'abc';
+    var string = `abc`;
     expect(string).to.be.a('string');
   });
   it('can evaluate variables', function() {
@@ -11,17 +11,17 @@ describe('template strings', function() {
     expect(string).to.equal('foobaz');
   });
   it('can execute functions', function() {
-    var string = 'The date is: ${new Date()}';
+    var string = `The date is: ${new Date()}`;
     var date = new Date();
     expect(string).to.equal('The date is: ' + date);
   });
   it('can evaluate expressions', function() {
-    var string = '1+2 = ${1+2}';
+    var string = `1+2=${1+2}`;
     expect(string).to.equal('1+2=3');
   });
   it('can handle multiline strings', function() {
-    var string = 'hello
-                  world';
-    expect(string).to.equal('hello\n              world');
+    var string = `hello
+            world`;
+                expect(string).to.equal('hello\n            world');
   });
 });
