4b291b16573c4798fcb15a2cf31f7fb7bb453d11
schmitz1193 <schmitz1193@gmail.com>
2016-01-26 08:38:14 -0600

    destructuring test

--- tests/let-const-block.js
+++ tests/let-const-block.js
@@ -12,4 +12,24 @@ describe('let', function() {
     expect(sum).to.equal(21);
   });
 });
-  
+
+describe('const', function() {
+  it('creates immutable variables', function() {
+    const x = 'one and only';
+    expect(x).to.equal('one and only');
+  });
+});
+
+describe('destructuring', function() {
+  it('make an assignment to an object', function() {
+    var x = {a: 1, b: 2};
+    var {a, b} = x;
+    expect({a, b}).to.equal({a: 1, b: 2});
+  });
+  it('can swap variables', function() {
+    var a = 1;
+    var b = 3;
+    var [a, b] = [b, a];
+  expect(b).to.equal(1);
+  });
+});
